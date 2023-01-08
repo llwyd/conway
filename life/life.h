4a39abafdc7f4de4a858c32eaa60a6a1c859e3cd
llwyd <tom@llwyd.io>
2023-01-08 17:05:27 +0000

    change resolution for desktop emulator

--- life/life.h
+++ life/life.h
@@ -1,10 +1,20 @@
 #ifndef _LIFE_H_
 #define _LIFE_H_
 
+#ifdef TARGET_DESKTOP
+/* These parameters are for the Desktop Emulator */
+#define LCD_COLUMNS ( 64 )
+#define LCD_ROWS    ( 8 )
+#define LCD_PAGES ( 8 )
+
+#else
 /* These parameters are for the SSD1306 Display */
 #define LCD_COLUMNS ( 128 )
 #define LCD_ROWS    ( 8 )
 #define LCD_PAGES ( 8 )
+
+#endif
+
 #define LCD_FULL_ROWS ( LCD_ROWS * LCD_PAGES )
 
 void Life_Init( void ( *fn)( void ) );
