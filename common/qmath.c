99c3930c7cf2ca95c3c00cc089dd472099679a97
llwyd <tom@llwyd.io>
2025-02-11 20:28:18 +0000

    unsigned qmath

--- common/qmath.c
+++ common/qmath.c
@@ -33,3 +33,33 @@ int16_t QMath_Div(int16_t a, int16_t b, uint16_t q)
     return result;
 }
 
+uint16_t QMath_UMul(uint16_t a, uint16_t b, uint16_t q)
+{
+    assert(q < MAX_Q);
+
+    uint16_t result = (uint16_t)(((uint32_t)a * (uint32_t)b) >> q);
+    return result;
+}
+
+uint16_t QMath_UAdd(uint16_t a, uint16_t b, uint16_t q)
+{
+    assert(q < MAX_Q);
+    
+    uint16_t result = a + b;
+    return result;
+}
+
+uint16_t QMath_USub(uint16_t a, uint16_t b, uint16_t q)
+{
+    assert(q < MAX_Q);
+    uint16_t result = a - b;
+    return result;
+}
+
+uint16_t QMath_UDiv(uint16_t a, uint16_t b, uint16_t q)
+{
+    assert(q < MAX_Q);
+    uint16_t result = (uint16_t)(((uint32_t)a << q)/ (uint32_t)b);
+    return result;
+}
+
