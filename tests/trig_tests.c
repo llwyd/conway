5c6976314f137da7df8880b556aa8c46560504a9
llwyd <tom@llwyd.io>
2025-02-16 20:37:49 +0000

    test floats

--- tests/trig_tests.c
+++ tests/trig_tests.c
@@ -1,5 +1,15 @@
 #include "trig.h"
 #include "unity.h"
+#include <math.h>
+
+typedef float float32_t;
+
+static float32_t Bin2Deg(uint8_t x)
+{
+    float result = (1.0 / (float)(1 << 7)) * (180.0/ M_PI) * (M_PI) * (float)x;
+
+    return result;
+}
 
 static void test_TRIG_ATan2_0Deg()
 {
@@ -7,9 +17,11 @@ static void test_TRIG_ATan2_0Deg()
     point_t a = {.x = 5, .y = 0};
     point_t b = {.x = 0, .y = 0};
 
-    uint8_t angle = TRIG_ATan2(&a, &b);
-
+    uint8_t angle = TRIG_ATan2(&a, &b); 
     TEST_ASSERT_UINT8_WITHIN(2, 0 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 0.0, f_angle);
 }
 
 static void test_TRIG_ATan2_45Deg()
@@ -21,6 +33,9 @@ static void test_TRIG_ATan2_45Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 1 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 45.0, f_angle);
 }
 
 static void test_TRIG_ATan2_90Deg()
@@ -32,6 +47,9 @@ static void test_TRIG_ATan2_90Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 2 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 90.0, f_angle);
 }
 
 static void test_TRIG_ATan2_135Deg()
@@ -43,6 +61,9 @@ static void test_TRIG_ATan2_135Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 3 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 135.0, f_angle);
 }
 
 static void test_TRIG_ATan2_180Deg()
@@ -54,6 +75,9 @@ static void test_TRIG_ATan2_180Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 4 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 180.0, f_angle);
 }
 
 static void test_TRIG_ATan2_225Deg()
@@ -65,6 +89,10 @@ static void test_TRIG_ATan2_225Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 5 * (1 << 5), angle);
+
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 225.0, f_angle);
 }
 
 static void test_TRIG_ATan2_270Deg()
@@ -76,6 +104,9 @@ static void test_TRIG_ATan2_270Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 6 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 270.0, f_angle);
 }
 
 static void test_TRIG_ATan2_315Deg()
@@ -87,6 +118,9 @@ static void test_TRIG_ATan2_315Deg()
     uint8_t angle = TRIG_ATan2(&a, &b);
 
     TEST_ASSERT_UINT8_WITHIN(2, 7 * (1 << 5), angle);
+
+    float32_t f_angle = Bin2Deg(angle);
+    TEST_ASSERT_FLOAT_WITHIN(2.0, 315.0, f_angle);
 }
 
 extern void TRIG_TestsRun(void)
