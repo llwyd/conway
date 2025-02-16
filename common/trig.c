4a4adf7a45f46d4b6b6b99217c51eba27d17d7f4
llwyd <tom@llwyd.io>
2025-02-16 20:28:17 +0000

    some trig unit tests and atan2 func

--- common/trig.c
+++ common/trig.c
@@ -2,7 +2,77 @@
 #include <stdbool.h>
 #include <assert.h>
 #include "trig.h"
+#include "qmath.h"
+
+#define LUT_LEN (16U)
+#define PI_RADS (0x80)
+
+#ifndef CORDIC_ITS
+#define CORDIC_ITS (4U)
+#endif
+
+const uint16_t lut_atanpi2[LUT_LEN] =
+{
+    8192, 4836, 2555, 1297,  651,  325,  162,   81,   40,   20,   10,
+    5,    2,    1,    0,    0
+};
+
+static int16_t ABS(int16_t x)
+{
+    const int16_t mask = x >> 15U;
+    x ^= mask;
+    const int16_t result = x - mask;
+    return result;
+}
 
 extern uint8_t TRIG_ATan2(point_t * a, point_t * b)
 {
+    int16_t diff_x = (int16_t)a->x - (int16_t)b->x;
+    int16_t diff_y = (int16_t)a->y - (int16_t)b->y;
+
+    int16_t abs_x = ABS(diff_x);
+    int16_t abs_y = ABS(diff_y);
+    uint16_t cordic_angle = 0;
+    int16_t d = -1;
+
+    assert(abs_x >= 0);
+    assert(abs_y >= 0);
+
+    point_t c =
+    {
+        .x = (abs_x << 4),
+        .y = (abs_y << 4),
+    };
+    
+    for(uint32_t idx = 0; idx < CORDIC_ITS; idx++)
+    {
+        point_t next;
+        if(c.y == 0)
+        {
+            break;
+        }
+        next.x = c.x - (d * (c.y >> idx)) ;
+        next.y = c.y + (d * (c.x >> idx)) ;
+
+        c = next;
+        
+        cordic_angle += (d < 0) ? lut_atanpi2[idx] : -lut_atanpi2[idx];
+        d = (c.y < 0) ? 1 : -1;
+    }
+    
+    uint8_t angle = Q_UDNSCALE(cordic_angle, 8);
+    if( (diff_y < 0) && (diff_x < 0))
+    {
+        angle -= PI_RADS;
+    }
+    else if(diff_x < 0)
+    {
+        angle = PI_RADS - angle;
+    }
+    else if(diff_y < 0)
+    {
+        angle = -angle;
+    }
+
+    return angle;
 }
--- tests/trig_tests.c
+++ tests/trig_tests.c
@@ -1,7 +1,18 @@
 #include "trig.h"
 #include "unity.h"
 
-void test_TRIG_ATan20()
+static void test_TRIG_ATan2_0Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 5, .y = 0};
+    point_t b = {.x = 0, .y = 0};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 0 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_45Deg()
 {
     TEST_ASSERT_TRUE(true);
     point_t a = {.x = 5, .y = 5};
@@ -9,10 +20,83 @@ void test_TRIG_ATan20()
 
     uint8_t angle = TRIG_ATan2(&a, &b);
 
-    TEST_ASSERT_EQUAL_UINT8(0, angle);
+    TEST_ASSERT_UINT8_WITHIN(2, 1 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_90Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 0, .y = 5};
+    point_t b = {.x = 0, .y = 0};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 2 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_135Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 0, .y = 5};
+    point_t b = {.x = 5, .y = 0};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 3 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_180Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 0, .y = 0};
+    point_t b = {.x = 5, .y = 0};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 4 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_225Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 0, .y = 0};
+    point_t b = {.x = 5, .y = 5};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 5 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_270Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 0, .y = 0};
+    point_t b = {.x = 0, .y = 5};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 6 * (1 << 5), angle);
+}
+
+static void test_TRIG_ATan2_315Deg()
+{
+    TEST_ASSERT_TRUE(true);
+    point_t a = {.x = 5, .y = 0};
+    point_t b = {.x = 0, .y = 5};
+
+    uint8_t angle = TRIG_ATan2(&a, &b);
+
+    TEST_ASSERT_UINT8_WITHIN(2, 7 * (1 << 5), angle);
 }
 
 extern void TRIG_TestsRun(void)
 {
-    RUN_TEST(test_TRIG_ATan20) ;
+    RUN_TEST(test_TRIG_ATan2_0Deg);
+    RUN_TEST(test_TRIG_ATan2_45Deg);
+    RUN_TEST(test_TRIG_ATan2_90Deg);
+    RUN_TEST(test_TRIG_ATan2_135Deg);
+    RUN_TEST(test_TRIG_ATan2_180Deg);
+    RUN_TEST(test_TRIG_ATan2_225Deg);
+    RUN_TEST(test_TRIG_ATan2_270Deg);
+    RUN_TEST(test_TRIG_ATan2_315Deg);
 }
