5a4906c84de970f11ab8d53649a408448eafa3d5
llwyd <tom@llwyd.io>
2025-02-18 21:19:34 +0000

    change macro

--- common/qmath.h
+++ common/qmath.h
@@ -13,7 +13,7 @@
 #define Q_UDNSCALE(X,Y) ((uint8_t)(((X)) >> (Y)))
 
 #define Q_NUM (15)
-#define Q_SCALE (7U)
+#define Q_SCALE (Q_NUM - 8U)
 
 int16_t QMath_Mul(int16_t a, int16_t b, uint16_t q);
 int16_t QMath_Add(int16_t a, int16_t b, uint16_t q);
