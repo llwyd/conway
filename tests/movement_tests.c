0a482da7366405458e892d0d4a08b886322f5ef7
llwyd <tom@llwyd.io>
2025-02-18 22:41:40 +0000

    tests for movement

--- tests/movement_tests.c
+++ tests/movement_tests.c
@@ -10,19 +10,27 @@ static void TRIG_Translate(point_t * const p, uint8_t angle)
     int16_t x = 0U;
     int16_t y = 0U;
     
+    uint8_t cos_angle = angle + DEG_90;    
     if(angle < DEG_180)
     {
-        x = QMath_Mul(0x100, qcos[angle], Q_NUM);
         y = QMath_Mul(0x100, qsin[angle], Q_NUM);
-        p->x += Q_DNSCALE(x, Q_SCALE);
         p->y += Q_DNSCALE(y, Q_SCALE);
     }
     else
     {
-        x = QMath_Mul(0x100, qcos[angle - DEG_180], Q_NUM);
         y = QMath_Mul(0x100, qsin[angle - DEG_180], Q_NUM);
-        p->x -= Q_DNSCALE(x, Q_SCALE);
         p->y -= Q_DNSCALE(y, Q_SCALE);
+    }
+
+    if(cos_angle < DEG_180)
+    {
+        x = QMath_Mul(0x100, qsin[cos_angle], Q_NUM);
+        p->x += Q_DNSCALE(x, Q_SCALE);
+    }
+    else
+    {
+        x = QMath_Mul(0x100, qsin[cos_angle - DEG_180], Q_NUM);
+        p->x -= Q_DNSCALE(x, Q_SCALE);
     }
 }
 
@@ -38,6 +46,21 @@ static void test_MOVE_N()
     TRIG_Translate(&p, angle);
 
     TEST_ASSERT_EQUAL_UINT8(5, p.x);
+    TEST_ASSERT_EQUAL_UINT8(4, p.y);
+}
+
+static void test_MOVE_NE()
+{
+    point_t p =
+    {
+        .x = 5,
+        .y = 5,
+    };
+    uint8_t angle = DEG_315;
+
+    TRIG_Translate(&p, angle);
+
+    TEST_ASSERT_EQUAL_UINT8(6, p.x);
     TEST_ASSERT_EQUAL_UINT8(4, p.y);
 }
 
@@ -116,12 +139,29 @@ static void test_MOVE_W()
     TEST_ASSERT_EQUAL_UINT8(5, p.y);
 }
 
+static void test_MOVE_NW()
+{
+    point_t p =
+    {
+        .x = 5,
+        .y = 5,
+    };
+    uint8_t angle = DEG_225;
+
+    TRIG_Translate(&p, angle);
+
+    TEST_ASSERT_EQUAL_UINT8(4, p.x);
+    TEST_ASSERT_EQUAL_UINT8(4, p.y);
+}
+
 extern void MOVE_TestsRun(void)
 {
     RUN_TEST(test_MOVE_N);
+    RUN_TEST(test_MOVE_NE);
     RUN_TEST(test_MOVE_E);
     RUN_TEST(test_MOVE_SE);
     RUN_TEST(test_MOVE_S);
     RUN_TEST(test_MOVE_SW);
     RUN_TEST(test_MOVE_W);
+    RUN_TEST(test_MOVE_NW);
 }
