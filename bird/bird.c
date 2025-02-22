336c796c72f56064ff75ef5841bee32b158485c7
llwyd <tom@llwyd.io>
2025-02-22 11:31:53 +0000

    this is it I think

--- bird/bird.c
+++ bird/bird.c
@@ -10,17 +10,17 @@ _Static_assert(LCD_ROWS == 8U, "must be u8");
 #define NUM_BIRDS (16U)
 
 #define SEP_RADIUS8 (0x04U)
-#define COH_RADIUS8 (0x0AU)
+#define COH_RADIUS8 (0x0FU)
 
-#define SEP_ANGLE 0x08U
+#define SEP_ANGLE 0x06U
 #define COH_ANGLE 0x01U;
-#define EDGE_ANGLE 0x0FU;
+#define EDGE_ANGLE 0x1FU;
 
 #define SPEED_INC (0x011d)
 #define DELTA_FRACT (0x0FFF)
 #define ALPHA_POINT (0x007F)
-#define ALPHA (0x007f)
-#define EDGE (0x0fU)
+#define ALPHA (0x00FF)
+#define EDGE (0x05U)
 
 typedef struct
 {
