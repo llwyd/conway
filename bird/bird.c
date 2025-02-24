6d0989d315ea254a9afb4946192376031f1197f9
llwyd <tom@llwyd.io>
2025-02-24 21:02:04 +0000

    works nicer without border exclusion

--- bird/bird.c
+++ bird/bird.c
@@ -232,11 +232,8 @@ static void CollectNearbyBirds8(bird_t * const current_bird, nearby_t * const ne
             
             if(IsPointInSquare8(b, c, square_size))
             {
-                if(bird[idx].state == BirdState_Idle)
-                {
-                    near_birds->bird[near_birds->num] = idx;
-                    near_birds->num++;
-                }
+                near_birds->bird[near_birds->num] = idx;
+                near_birds->num++;
             }
         }
     }
