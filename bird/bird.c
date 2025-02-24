df00ef1584c159edfa93f7a7561a3d289ff355ae
llwyd <tom@llwyd.io>
2025-02-24 20:08:17 +0000

    looks cool with 32

--- bird/bird.c
+++ bird/bird.c
@@ -7,7 +7,7 @@ _Static_assert(LCD_PAGES <= UINT8_MAX, "invalid num of pages");
 _Static_assert(LCD_COLUMNS <= UINT8_MAX, "invalid cols");
 _Static_assert(LCD_ROWS == 8U, "must be u8");
 
-#define NUM_BIRDS (16U)
+#define NUM_BIRDS (32U)
 
 #define SEP_RADIUS8 (0x04U)
 #define COH_RADIUS8 (0x0FU)
