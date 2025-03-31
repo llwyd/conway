1a7a8826c3bc54a41501dcea41ee7e1e4d77d2b6
llwyd <tom@llwyd.io>
2025-03-31 20:57:45 +0100

    looks nicer

--- bird/bird.c
+++ bird/bird.c
@@ -13,8 +13,8 @@ _Static_assert(LCD_ROWS == 8U, "must be u8");
 #define MAX_NEARBY (16U)
 
 /* 0.175 ~= 0x1666 */
-#define COH_RADIUS8 (0x1FFF)
-#define SEP_RADIUS8 (COH_RADIUS8 >> 6)
+#define COH_RADIUS8 (0x3FFF)
+#define SEP_RADIUS8 (COH_RADIUS8 >> 5)
 
 _Static_assert(COH_RADIUS8 > 0, "Must be > 0");
 _Static_assert(SEP_RADIUS8 > 0, "Must be > 0");
@@ -22,7 +22,7 @@ _Static_assert(COH_RADIUS8 > SEP_RADIUS8, "Coh > Sep");
 
 #define SEP_ANGLE   (0x0F)
 #define COH_ANGLE   (0x01)
-#define EDGE_ANGLE  (0x14)
+#define EDGE_ANGLE  (0x08)
 
 _Static_assert(SEP_ANGLE > 0, "Must be > 0");
 _Static_assert(COH_ANGLE > 0, "Must be > 0");
@@ -34,7 +34,7 @@ _Static_assert(COH_ANGLE < SEP_ANGLE, "Must be < 0");
 #define ALPHA (0x007F)
 
 /* 0.075 ~= 0x0999 */
-#define EDGE (0x0999)
+#define EDGE (0x0799)
 _Static_assert(EDGE > 0, "Must be > 0");
 
 typedef struct
