4498cbae0f5f31367fb99832f4836d9f272dc00c
llwyd <tom@llwyd.io>
2025-03-31 21:04:33 +0100

    looking good

--- bird/bird.c
+++ bird/bird.c
@@ -14,13 +14,13 @@ _Static_assert(LCD_ROWS == 8U, "must be u8");
 
 /* 0.175 ~= 0x1666 */
 #define COH_RADIUS8 (0x3FFF)
-#define SEP_RADIUS8 (COH_RADIUS8 >> 5)
+#define SEP_RADIUS8 (COH_RADIUS8 >> 4)
 
 _Static_assert(COH_RADIUS8 > 0, "Must be > 0");
 _Static_assert(SEP_RADIUS8 > 0, "Must be > 0");
 _Static_assert(COH_RADIUS8 > SEP_RADIUS8, "Coh > Sep");
 
-#define SEP_ANGLE   (0x0F)
+#define SEP_ANGLE   (0x07)
 #define COH_ANGLE   (0x01)
 #define EDGE_ANGLE  (0x08)
 
