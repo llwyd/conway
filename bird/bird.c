9e62337d434ef39d9c276ab47743aad8fad20b94
llwyd <tom@llwyd.io>
2025-03-31 22:29:43 +0100

    looks more bird like

--- bird/bird.c
+++ bird/bird.c
@@ -13,7 +13,7 @@ _Static_assert(LCD_ROWS == 8U, "must be u8");
 #define MAX_NEARBY (16U)
 
 /* 0.175 ~= 0x1666 */
-#define COH_RADIUS8 (0x3FFF)
+#define COH_RADIUS8 (0x0FFF)
 #define SEP_RADIUS8 (COH_RADIUS8 >> 4)
 
 _Static_assert(COH_RADIUS8 > 0, "Must be > 0");
@@ -21,7 +21,7 @@ _Static_assert(SEP_RADIUS8 > 0, "Must be > 0");
 _Static_assert(COH_RADIUS8 > SEP_RADIUS8, "Coh > Sep");
 
 #define SEP_ANGLE   (0x07)
-#define COH_ANGLE   (0x01)
+#define COH_ANGLE   (0x03)
 #define EDGE_ANGLE  (0x08)
 
 _Static_assert(SEP_ANGLE > 0, "Must be > 0");
@@ -31,7 +31,7 @@ _Static_assert(COH_ANGLE < SEP_ANGLE, "Must be < 0");
 
 #define SPEED_INC (0x05FF)
 #define ALPHA_POINT (0x00FF)
-#define ALPHA (0x000F)
+#define ALPHA (0x003F)
 
 /* 0.075 ~= 0x0999 */
 #define EDGE (0x0799)
